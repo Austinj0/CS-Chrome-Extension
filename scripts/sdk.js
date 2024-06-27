230366b59e5d2ad1ffa8aa0e017c9ebe5bb43200
Austin Jones <austin@alloy.com>
2024-06-26 21:31:30 -0400

    mockup of sdk features

---
+++ scripts/sdk.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function () { 
+    /*
+    const getParamValue = (paramName, url) => {
+        // Create a URL object from the provided URL
+        const urlObj = new URL(url);
+        
+        // Use the URLSearchParams object to get the value of the parameter
+        const paramValue = urlObj.searchParams.get(paramName);
+        
+        // Return the parameter value
+        return paramValue;
+    }
+    setTimeout(function(){
+        const sdk = document.querySelector("#alloyIframe");
+        const ja = getParamValue("journeyApplicationToken", sdk.src);
+        const journey = getParamValue("journeyToken", sdk.src);
+        const key = getParamValue("key", sdk.src);
+        const sdkDetails = {
+            ja : ja,
+            journey: journey,
+            key: key
+        }
+        console.log(sdkDetails)
+    }, 5000);
+    */
+})
